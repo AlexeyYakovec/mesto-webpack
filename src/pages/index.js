cba4a1539610a0cc45f271b14ddd0b9c6b8b36e3
AlexeyYakovec <yakovec8952@gmail.com>
2024-03-12 10:43:39 +0500

    full project

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,22 @@ import { initialCards } from "../scripts/initialCards";
 
 import "../pages/index.css";
 
+/* profile */
+const profileEditPopup = document.querySelector(".popup_type_edit-profile");
+const profileInputName = document.querySelector(".popup__input_name");
+const profileInputJob = document.querySelector(".popup__input_job");
+const profileName = document.querySelector(".profile__name");
+const profileJob = document.querySelector(".profile__job");
+
+/* add card */
+const addCardPopup = document.querySelector(".popup_type_add-card");
+const addCardInputName = document.querySelector(".popup__input_image-name");
+const addCardInputLink = document.querySelector(".popup__input_image-link");
+
+/* buttons */
+const profileEditButton = document.querySelector(".profile__edit-button");
+const AddCardButton = document.querySelector(".profile__add-button");
+
 /* open image view */
 const popupImageView = document.querySelector(".popup_type_image-view");
 const popupImage = document.querySelector(".popup__image");
@@ -90,3 +106,36 @@ function handleOverlayClick(e) {
     closePopup(popupOpen);
   }
 }
+
+function editProfile() {
+  openPopup(profileEditPopup);
+  profileInputName.value = profileName.textContent;
+  profileInputJob.value = profileJob.textContent;
+}
+
+function handleCardFormSubmit(e) {
+  e.preventDefault();
+
+  cardsContainer.prepend(
+    addPhoto(addCardInputName.value, addCardInputLink.value)
+  );
+
+  formAddCard.reset();
+  closePopup(addCardPopup);
+}
+
+function handleProfileFormSubmit(e) {
+  e.preventDefault();
+
+  profileName.textContent = profileInputName.value;
+  profileJob.textContent = profileInputJob.value;
+  closePopup(profileEditPopup);
+}
+
+AddCardButton.addEventListener("click", () => {
+  openPopup(addCardPopup);
+});
+
+profileEditButton.addEventListener("click", editProfile);
+formProfile.addEventListener("submit", handleProfileFormSubmit);
+formAddCard.addEventListener("submit", handleCardFormSubmit);
